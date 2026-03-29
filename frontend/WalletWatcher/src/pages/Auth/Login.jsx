fe0b0dc87b9f82631b205e800603295f1c434937
Vaishnavi <emailvaishnaviydv0110@proton.me>
2026-03-29 13:57:48 +0530

    AuthLayout, Login Page UI, validateEmail function. Everything is responsive. Used React icon for logo instead of AI made assets.

--- frontend/WalletWatcher/src/pages/Auth/Login.jsx
+++ frontend/WalletWatcher/src/pages/Auth/Login.jsx
@@ -1,16 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import Input from "../../components/Inputs/Input";
+import {Link, useNavigate} from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
-    return (
-        <div>
-            <div className = "lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-                <h3 className = "text-xl font-semibold text-black">Welcome Back!</h3>
-                <p className = "text-xs text-slate-700 mt-[5px] mb-6">
-                    Please enter your details to log in.
-                </p>
-            </div>
-        </div>
-    )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
-export default Login
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if (!password) {
+            setError("Please enter the password");
+            return;
+        }
+
+        setError("");
+
+        // Login API later
+    }
+
+    return (
+        <AuthLayout>
+            <div className="w-full lg:w-[70%] flex flex-col justify-center min-h-[70vh] md:min-h-full">
+            {/* Fixed above using AI <div className = "lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center"> */} 
+                <h3 className = "text-xl font-bold text-black">Welcome Back!</h3>
+                <p className = "text-xs text-slate-700 mt-[5px] mb-6">
+                    Let's log you in.
+                </p>
+
+                <form onSubmit = {handleLogin}>
+                    <Input
+                        value = {email}
+                        onChange = {({target}) => setEmail(target.value)}
+                        label={<span className="font-semibold">Email Address</span>}
+                        placeholder = "jane@example.com"
+                        type = "text"
+                    />
+
+                    <Input
+                        value = {password}
+                        onChange = {({target}) => setPassword(target.value)}
+                        label={<span className="font-semibold">Password</span>}
+                        placeholder = "Min. 8 Characters"
+                        type = "password"
+                    />
+
+                    {error && <p className = "text-red-500 text-xs pb-2.5">{error}</p>}
+
+                    <button type = "submit" className = "btn-primary">
+                        Login
+                    </button>
+
+                    <p className = "text-[13px] text-slate-800 mt-3">
+                        Don't have an account yet? {""}
+                        <Link className = "font-medium text-primary underline" to = "/signup">
+                            SignUp Now
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </AuthLayout>
+    );
+};
+
+export default Login;
